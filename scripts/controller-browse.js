ad55703e4c216e458937ecaf65e943dcc0ae0f01
Brian best <brian.brianb64@gmail.com>
2015-05-10 16:05:52 -0300

    tasks are showing up staticly. ngshow/hide error has been fixed

--- scripts/controller-browse.js
+++ scripts/controller-browse.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.controller('BrowseController', function($scope,Auth){
-	
+	$scope.task = 0;
 	if(Auth.signedIn){
 		
 		$('#modal-login').modal('show');
@@ -13,18 +13,33 @@ app.controller('BrowseController', function($scope,Auth){
 		if($scope.regNew){
 			console.log('registering new user');
 			Auth.register(user).then(function(){
-				$('#modal-login').toggle();
+				$('#modal-login').modal('hide');
 			},function(err) {
 				alert(err);
 			});
 		}else{
 			console.log('loging in user');
 		 	Auth.login(user).then(function(){
-				$('#modal-login').toggle();
+				$('#modal-login').modal('hide');
 			},function(err) {
 				alert(err);
 			});
 		}
+	};
+	
+	$scope.addTask = function(task) {
+		//Get username
+		task.user = Auth.user;
+		//Get task
+		//Get current time
+		task.time = new Date().getTime();
+		//Add to firebase
+		$scope.task = task;
+		
+		
+	}
+	$scope.team = function() {
+		
 	}
 	
 });
