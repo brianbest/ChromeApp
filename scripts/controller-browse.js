06cfe78135cf6fe459ee98943c9e21054fa74c3d
Brian best <brian.brianb64@gmail.com>
2015-05-10 15:17:35 -0300

    firebase connection works and registering users too

--- scripts/controller-browse.js
+++ scripts/controller-browse.js
@@ -2,5 +2,29 @@
 
 app.controller('BrowseController', function($scope,Auth){
 	
+	if(Auth.signedIn){
+		
+		$('#modal-login').modal('show');
+		
+	}
+	
+	$scope.regNew = false;
+	$scope.login = function(user){
+		if($scope.regNew){
+			console.log('registering new user');
+			Auth.register(user).then(function(){
+				$('#modal-login').toggle();
+			},function(err) {
+				alert(err);
+			});
+		}else{
+			console.log('loging in user');
+		 	Auth.login(user).then(function(){
+				$('#modal-login').toggle();
+			},function(err) {
+				alert(err);
+			});
+		}
+	}
 	
 });
