88823af0e17b0f1efb7ea3a334d69c9885e71f12
Brian best <brian.brianb64@gmail.com>
2015-05-10 12:25:05 -0300

    extention now overwrites new tab

--- background.js
+++ background.js
@@ -1,8 +1,21 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
+    chrome.tabs.create({ url: newURL });
 });
+
+//chrome.app.runtime.onLaunched.addListener(function() {
+//  chrome.app.window.create('window.html', {
+//    'bounds': {
+//      'width': 400,
+//      'height': 500
+//    }
+//  });
+//});
+//
+//chrome.browserAction.onClicked.addListener(function(tab) {
+//  chrome.tabs.create({'url': chrome.extension.getURL('f.html')}, function(tab) {
+//    // Tab opened.
+//    console.log('hello');
+//  });
+//});
